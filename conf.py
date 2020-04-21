69ebfff4cf3b02a8fbf1bca12f69c447d5215081
qingexiao <63902272+qingexiao@users.noreply.github.com>
2020-04-21 15:19:31 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "清歌笑'Blog"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "清歌笑"
+email = "lost@ver.red"
+author_homepage = "https://www.mobri.cn"
+description = "我自是年少，韶华倾负。"
+key_words = ['Maverick', '清歌笑', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
